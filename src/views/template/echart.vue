a87ff33a4c05c402ed5c8992d9ad110b592c08be
“用e <1378576508@qq.com>
2020-04-10 18:31:25 +0800

    为

--- src/views/template/echart.vue
+++ src/views/template/echart.vue
@@ -1,6 +1,8 @@
 
 <template>
   <div class="chart-container">
+        <div id="main" style="width:100%;height: 100%"></div>
+
     <div id="main" style="width:100%;height: 100%"></div>
   </div>
 </template>
